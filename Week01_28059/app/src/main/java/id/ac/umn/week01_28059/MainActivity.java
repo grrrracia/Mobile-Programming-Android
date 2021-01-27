b33805f5b7a02d401f3b44928c52bb5ea210aeb6
Gracia Angelica N <61301424+grrrracia@users.noreply.github.com>
2021-01-27 15:59:10 +0700

    Week 1

---
+++ Week01_28059/app/src/main/java/id/ac/umn/week01_28059/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.week01_28059;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
